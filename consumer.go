b778a8e001576f6bc1bef072a78200a9af425458
Swayam <158766919+Swayamjimmy@users.noreply.github.com>
2026-01-06 19:21:12 +0530

    smtp mailpit service

--- consumer.go
+++ consumer.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/smtp"
 	"sync"
+	"time"
 )
 
 func emailWorker(id int, ch chan Recipient, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for recipient := range ch {
-		fmt.Printf("Worker %d sending email to %s\n", id, recipient.Email)
+
+		smtpHost := "localhost"
+		smtpPort := "1025"
+
+		formattedMsg := fmt.Sprintf("To: %s\r\nSubject: Test Email\r\n\r\n%s", recipient.Email, "Hello, this is a test email from Snailmail")
+
+		msg := []byte(formattedMsg)
+
+		err := smtp.SendMail(smtpHost+":"+smtpPort, nil, "snailmail@example.com", []string{recipient.Email}, msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		time.Sleep(50 * time.Millisecond)
+
+		fmt.Printf("Worker %d sent email to %s\n", id, recipient.Email)
+
 	}
 }
